225bf938e167027ee6a2870147173226721e2814
JhoVie <34365455+JhorAVi@users.noreply.github.com>
2022-07-14 21:23:34 +0800

    Exclude trailing_stop_loss
    Request: Excluded trailing_stop_loss from being handled by StoplossGuard.
    Reasons:
         1. Unlike regular stoploss, a trailing_stop_loss often returns a micro loss (eg -0.12%). It doesn't make sense to  suspended that pair (or all pairs) because of that little loss.
         2. trailing_stop_loss (going up) is often used to ride a green candle therefore a hard stoploss (going down) is better fit for StoplossGuard.

--- freqtrade/plugins/protections/stoploss_guard.py
+++ freqtrade/plugins/protections/stoploss_guard.py
@@ -47,7 +47,7 @@ class StoplossGuard(IProtection):
 
         trades1 = Trade.get_trades_proxy(pair=pair, is_open=False, close_date=look_back_until)
         trades = [trade for trade in trades1 if (str(trade.exit_reason) in (
-            ExitType.TRAILING_STOP_LOSS.value, ExitType.STOP_LOSS.value,
+            ExitType.STOP_LOSS.value,
             ExitType.STOPLOSS_ON_EXCHANGE.value)
             and trade.close_profit and trade.close_profit < 0)]
 
